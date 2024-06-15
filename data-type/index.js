011200aaa57a25bb93fcc5c481cbfee2d81cd240
YongChenSu <yongchensu0@gmail.com>
2024-06-15 14:38:08 +0800

    add data-type

---
+++ data-type/index.js
@@ -0,0 +1,12 @@
+// 基本資料型態的變數宣告和驗證
+// let x:number;
+// x=10;
+// console.log(x)
+// let s:string="Hello";
+// console.log(s);
+// 陣列資料型態的變數宣告和驗證
+var grades = [60, 70, 80];
+grades.push(100);
+var a;
+a = [true, false, 10];
+console.log(a);
---
+++ data-type/index.ts
@@ -0,0 +1,14 @@
+// 基本資料型態的變數宣告和驗證
+let x:number;
+x=10;
+console.log(x)
+let s:string="Hello";
+console.log(s);
+
+// 陣列資料型態的變數宣告和驗證
+let score:number[]=[60,70,80];
+score.push(100);
+
+let a: Array<boolean>
+a=[true,false,true];
+console.log(a);
